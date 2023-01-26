245ed4c97eb555aa4644708a0ba75742016bc9c4
ERTUĞRUL YILMAZ <ertugrul.yilmaz.bal@gmail.com>
2023-01-26 15:08:02 +0300

    Pipes learned and cart feature has implemented.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +15,12 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded = false;
+  filterText = "";
 
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void{
     
@@ -40,6 +46,11 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     })
+  }
+
+  addToChart(product:Product){
+    this.toastrService.success("Added", product.productName);
+    this.cartService.addToCart(product);
   }
 
 }
